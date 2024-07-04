f59020251dfb2c9089b695a0391e35a4cc613351
Hyunhopark1 <phh1569@nate.com>
2024-07-04 17:28:08 +0900

    add

---
+++ myjpa/src/test/java/com/maven/springboot/myjpa/MyjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.springboot.myjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
